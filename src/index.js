8661b7cfa6adf75eb3fe3237cd96fcf9f8823f61
bence.bognar <bence.bognar@dpc.hu>
2019-01-23 11:34:50 +0100

    Store connected

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import 'onsenui/css/onsen-css-components.css';
 import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+import {rootReducer} from "./store";
+import {Provider} from "react-redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, document.getElementById('root'));
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
