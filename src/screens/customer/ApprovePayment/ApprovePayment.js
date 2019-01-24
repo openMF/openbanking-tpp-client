493be5fb2a4064f06335df9712ec3f1b6b326603
Attila Sedon <attila.sedon@dpc.hu>
2019-01-24 13:16:31 +0100

    delete console.log, and helper function

--- src/screens/customer/ApprovePayment/ApprovePayment.js
+++ src/screens/customer/ApprovePayment/ApprovePayment.js
@@ -6,33 +6,8 @@ import {Card} from "react-onsenui";
 import Button from "react-onsenui/src/components/Button";
 
 class ApprovePayment extends PureComponent {
-    renderCard = (o) => {
-        console.log('o: ', o);
-        return Object.keys(o).map(key => {
-            console.log('key: ', key);
-            console.log('o[key]: ', o[key]);
-            if(typeof o[key] !== 'object') {
-                return (
-                    <Card>
-                        <p>{key}: {o[key]}</p>
-                    </Card>
-                )
-            } else {
-                return Object.keys(o[key]).map(key2 => {
-                    return (
-                        <Card>
-                            <p>{key2}: {o[key][key2]}</p>
-                        </Card>
-                    )
-                })
-            }
-
-        })
-    };
-
     render() {
         const {data} = this.props;
-        console.log('this.renderCard: ', this.renderCard(data));
         return (<Layout>
             <h1>ApprovePayment</h1>
             <Card>
