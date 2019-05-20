cc020ab8a9e668eb433d5e56f8dd3988dc5fcb66
NAdamo <adam.nemeth@dpc.hu>
2019-05-20 15:54:39 +0200

    fixed when on succesful response the merchant request payment generation not navigated to the success screen

--- src/store/payment/thunks.js
+++ src/store/payment/thunks.js
@@ -66,9 +66,10 @@ export const fetchPaymentSuccess = (history, qrData) => (dispatch, getState) => 
     axios.get(`${getServerUrl(bank)}/client/${qrData.clientRefId}`).then(
         response => {
             if (response.data.transferState === 'COMMITTED') {
+                const payerId = response.data.originalRequestData ? response.data.originalRequestData.payer.partyIdInfo.partyIdentifier : "27710203999"
                 dispatch(setPaymentSuccess(
                     response.data.transactionId,
-                    response.data.originalRequestData.payer.partyIdInfo.partyIdentifier
+                    payerId
                 ));
                 history.push(`/merchant/paymentComplete`);
             }
